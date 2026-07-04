34b8aae8ed4625e4c68af3e3c07dd0df83b093e5
Aditya <adithakare40@gmaail.com>
2026-07-04 18:08:11 +0000

    Add automatic EC2 deployment

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from DevOps CI/CD Project 🚀");
+    res.send("Hello from DevOps CI/CD Project 🚀 version 2");
 });
 
 const PORT = process.env.PORT || 3000;
